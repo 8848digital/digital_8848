3f440754ab68e0c3783b4c5cc3a5f9160678f92f
aartimanjare <aartimanjare@gmail.com>
2024-11-14 18:43:23 +0530

    fix: added keywords and change field types

--- digital_8848/digital_8848/doctype/meta_tags/meta_tags.py
+++ digital_8848/digital_8848/doctype/meta_tags/meta_tags.py
@@ -1,9 +1,10 @@
 # Copyright (c) 2024, digital_8848 and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class MetaTags(Document):
-	pass
+    def before_save(self):
+        if self.meta_image:
+            self.open_graph_images = self.meta_image
